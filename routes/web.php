2af15a2efa425463110955b9c0b1a3403461faf3
TomChrysostem <toeyadanar.tya@gmail.com>
2020-10-03 18:58:11 +0630

    auth-lesson

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
 
-Route::resource('students', 'StudentController');
-Route::resource('teachers', 'TeacherController');
+Route::get('/home', 'HomeController@index')->name('home');
